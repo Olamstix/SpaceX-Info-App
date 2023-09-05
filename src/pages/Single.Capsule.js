7c6c3bc8c1696e6d459f5016a657d6a841df988a
Olamstix <olamstix@gmail.com>
2023-09-05 09:59:24 +0100

    Fix the active and retired status on Single Capsule

--- src/pages/Single.Capsule.js
+++ src/pages/Single.Capsule.js
@@ -22,6 +22,7 @@ export default function SingleCapsule() {
         <Loading />
       ) : (
         <section className="py-32 max-width grid grid-cols-1 gap-8 md:grid-cols-2">
+          {/* {singleCapsule.map(({status}) => ( */}
           <article>
             <h1 className="heading mt-50">{singleCapsule.type}</h1>
             <h2 className="capitalize text-3xl opacity-75 mt-2 text-white font-bold">
@@ -32,18 +33,18 @@ export default function SingleCapsule() {
             </h2>
             <div className="grid grid-cols-1 gap-5 md:grid-cols-2 text-white opacity-75">
               <ul>
+                <li>Land Landings: {singleCapsule.land_landings}</li>
                 <li>
                   Launches: {singleCapsule.launches}
                 </li>
-                <li>Land Landings: {singleCapsule.land_landings}</li>
-              </ul>
-              <ul>
-                <li>Reuse Count: {singleCapsule.reuse_count}%</li>
-                {singleCapsule.active ? (
+                {singleCapsule.status === "active" ? (
                   <li className="text-emerald-500">Active</li>
                 ) : (
                   <li className="text-rose-500">Retired</li>
                 )}
+              </ul>
+              <ul>
+                <li>Reuse Count: {singleCapsule.reuse_count}%</li>
               </ul>
             </div>
             <ul className="flex items-center justify-start gap-3 mt-5">
@@ -57,6 +58,7 @@ export default function SingleCapsule() {
               </li>
             </ul>
           </article>
+          {/* ))} */}
         </section>
       )}
     </>
